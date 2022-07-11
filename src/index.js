e30c86ae270854e2ac9074f4d9d67ba73a68ec2d
Gabriel Terrazas <95561135+Kombini21@users.noreply.github.com>
2022-07-11 14:05:21 -0400

    Add deleteCard function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import {ToDo} from './toDoFactory';
 
 
 var todo1 = ToDo("casa","casa1","casa1","casa1","casa1","casa1","casa1");
+var todo2 = ToDo("casa","casa1s","casa12","casa12","casa1","casa1","casa1");
 
 todo1.createCard();
+todo2.createCard();
+
+todo2.deleteCard();
+todo1.deleteCard();
+
+
--- src/toDoFactory.js
+++ src/toDoFactory.js
@@ -1,5 +1,4 @@
 // This is a factory to create each To do List card.
-
 export const ToDo = (title, description, dueDate, priority, notes, classe, checklist) => {
 
     // Atribui os valores a cada variavel 
@@ -10,7 +9,8 @@ export const ToDo = (title, description, dueDate, priority, notes, classe, check
    var priority = priority;
    var notes = notes;
    var classe = classe;
-   var checklist = checklist; 
+   var checklist = checklist;
+   var id = Math.floor(Math.random() * 10000);
 
     // Cria o card no DOM
    const createCard = () => {
@@ -21,39 +21,55 @@ export const ToDo = (title, description, dueDate, priority, notes, classe, check
 
        const containerCard = document.createElement("div");
        containerCard.setAttribute('class','containercards');
+       containerCard.setAttribute('id',`${id}`);
        
        const divTitle = document.createElement('div');
        divTitle.setAttribute('class','title');
+       divTitle.innerHTML = title;
 
        const divDescription = document.createElement('div');
        divDescription.setAttribute('class','description');
+       divDescription.innerHTML = description;
 
        const divDueDate = document.createElement('div');
        divDueDate.setAttribute('class', 'duedate');
+       divDueDate.innerHTML = dueDate;
 
        const divPriority = document.createElement('div');
        divPriority.setAttribute('class', 'priority');
+       divPriority.innerHTML = priority;
 
-       const notes = document.createElement('div');
-       notes.setAttribute('class', 'notes');
+       const divNotes = document.createElement('div');
+       divNotes.setAttribute('class', 'notes');
+       divNotes.innerHTML = notes;
 
-       const classe = document.createElement('div');
-       classe.setAttribute('class', 'classe');
+       const divClasse = document.createElement('div');
+       divClasse.setAttribute('class', 'classe');
+       divClasse.innerHTML = classe;
 
-       const checklist = document.createElement('div');
-       checklist.setAttribute('class', 'checklist');
+       const divChecklist = document.createElement('div');
+       divChecklist.setAttribute('class', 'checklist');
+       divChecklist.innerHTML = checklist;
 
        containerCard.appendChild(divTitle);
        containerCard.appendChild(divDescription);
        containerCard.appendChild(divDueDate);
        containerCard.appendChild(divPriority);
-       containerCard.appendChild(notes);
-       containerCard.appendChild(classe);
-       containerCard.appendChild(checklist);
+       containerCard.appendChild(divNotes);
+       containerCard.appendChild(divClasse);
+       containerCard.appendChild(divChecklist);
        container.appendChild(containerCard);
    }
 
-   return {createCard};
+
+   const deleteCard = () => {
+
+    const container = document.getElementById(id);
+    container.innerHTML = '';
+
+   }
+
+   return {createCard, deleteCard};
 };
 
 
